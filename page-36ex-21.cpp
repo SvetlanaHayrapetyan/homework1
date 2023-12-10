43f5b94d02bef71389d7a4daca908b8c6a77d808
SvetlanaHayrapetyan <153418228+SvetlanaHayrapetyan@users.noreply.github.com>
2023-12-10 12:01:49 +0400

    page-36ex-21.cpp

---
+++ page-36ex-21.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main ()
+{
+    double a;
+    const double pi = 3.141592653589793;
+    
+    std::cin >> a; 
+    
+    std::cout << "Length of circle = " << a * pi << std::endl;
+    
+    
+    return 0;
+}
